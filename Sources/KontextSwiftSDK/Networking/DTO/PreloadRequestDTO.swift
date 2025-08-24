e6cc4878060222dc21c2893e7c8322a5fbc5d736
Michal Štembera <stembera.michal@gmail.com>
2025-08-24 15:12:48 +0200

    Add regulatory as a paramater to AdsProviderConfiguration.

--- Sources/KontextSwiftSDK/Networking/DTO/PreloadRequestDTO.swift
+++ Sources/KontextSwiftSDK/Networking/DTO/PreloadRequestDTO.swift
@@ -9,42 +9,15 @@ struct PreloadRequestDTO: Encodable {
     let userId: String
     let messages: [MessageDTO]
     let variantId: String?
-    let character: Character?
+    let character: CharacterDTO?
     let advertisingId: String?
     let vendorId: String?
     let sessionId: String?
     let device: DeviceDTO?
     let sdk: String
     let sdkVersion: String
-    
-    init(
-        publisherToken: String,
-        conversationId: String,
-        userId: String,
-        messages: [MessageDTO],
-        variantId: String?,
-        character: Character?,
-        advertisingId: String?,
-        vendorId: String?,
-        sessionId: String?,
-        device: DeviceDTO?,
-        sdk: String,
-        sdkVersion: String
-    ) {
-        self.publisherToken = publisherToken
-        self.conversationId = conversationId
-        self.userId = userId
-        self.messages = messages
-        self.variantId = variantId
-        self.character = character
-        self.advertisingId = advertisingId
-        self.vendorId = vendorId
-        self.sessionId = sessionId
-        self.device = device
-        self.sdk = sdk
-        self.sdkVersion = sdkVersion
-    }
-    
+    let regulatory: RegulatoryDTO?
+
     init(
         sessionId: String?,
         configuration: AdsProviderConfiguration,
@@ -53,19 +26,18 @@ struct PreloadRequestDTO: Encodable {
         sdk: String,
         sdkVersion: String
     ) {
-        self.init(
-            publisherToken: configuration.publisherToken,
-            conversationId: configuration.conversationId,
-            userId: configuration.userId,
-            messages: messages.map(MessageDTO.init),
-            variantId: configuration.variantId,
-            character: configuration.character,
-            advertisingId: configuration.advertisingId,
-            vendorId: configuration.vendorId,
-            sessionId: sessionId,
-            device: DeviceDTO(from: device),
-            sdk: sdk,
-            sdkVersion: sdkVersion
-        )
+        publisherToken = configuration.publisherToken
+        conversationId = configuration.conversationId
+        userId = configuration.userId
+        self.messages = messages.map(MessageDTO.init)
+        variantId = configuration.variantId
+        character = CharacterDTO(from: configuration.character)
+        advertisingId = configuration.advertisingId
+        vendorId = configuration.vendorId
+        self.sessionId = sessionId
+        self.device = DeviceDTO(from: device)
+        self.sdk = sdk
+        self.sdkVersion = sdkVersion
+        self.regulatory = RegulatoryDTO(from: configuration.regulatory)
     }
 }
