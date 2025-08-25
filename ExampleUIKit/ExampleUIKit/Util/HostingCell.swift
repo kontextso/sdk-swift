72b3d46d4180bdf67cb35c2573dff2b6197b86d7
Dominika Gajdova <dominika.gajdova@gnj.cz>
2025-08-25 16:34:48 +0800

    chore: attempt to fix uikit sizing

---
+++ ExampleUIKit/ExampleUIKit/Util/HostingCell.swift
@@ -0,0 +1,8 @@
+//
+//  HostingCell.swift
+//  ExampleUIKit
+//
+//  Created by Dominika Gajdová on 25.08.2025.
+//
+
+import Foundation
---
+++ Sources/KontextSwiftSDK/InlineAd/InlineAdUIView.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  KontextSwiftSDK
+//
+//  Created by Dominika Gajdová on 25.08.2025.
+//
+
+import Foundation
