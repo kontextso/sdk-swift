e3deb9281fa797268bbdfd75b0abe057d29116c1
JakubMrozek <jakub.mrozek@gmail.com>
2026-02-25 06:07:50 +0800

    fix with support iOS 16

--- Sources/KontextSwiftSDK/Model/Info/OSInfo.swift
+++ Sources/KontextSwiftSDK/Model/Info/OSInfo.swift
@@ -24,24 +24,11 @@ final class OSInfo: Sendable {
 
     @MainActor
     static func current() -> OSInfo {
-        let localeObj = Locale.current
-        let language = localeObj.language.languageCode?.identifier
-        let region = localeObj.language.region?.identifier
-
-        let bcp47locale = [language, region].compactMap { $0 }.joined(separator: "-")
-        
-        let fallback = Locale.current
-            .identifier
-            .replacingOccurrences(of: "_", with: "-")
-        
-        let finalLocale = bcp47locale.isEmpty ? fallback : bcp47locale
-        
-        return OSInfo(
+        OSInfo(
             name:  UIDevice.current.systemName.lowercased(),
             version: UIDevice.current.systemVersion,
-            locale: finalLocale,
+            locale: Locale.current.identifier.replacingOccurrences(of: "_", with: "-"),
             timezone: TimeZone.current.identifier
         )
     }
 }
-
