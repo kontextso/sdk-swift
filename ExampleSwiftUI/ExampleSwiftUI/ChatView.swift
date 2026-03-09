d527aaba76e7b78e0de9178ba014c07bcbb66d20
JakubMrozek <jakub.mrozek@gmail.com>
2026-03-09 20:01:13 +0500

    test strings

--- ExampleSwiftUI/ExampleSwiftUI/ChatView.swift
+++ ExampleSwiftUI/ExampleSwiftUI/ChatView.swift
@@ -103,6 +103,8 @@ struct ChatView: View {
             id: UUID().uuidString,
             role: .user,
             // content: "Hello my smart helpful assistant, how are you?",
+            // content: "kontextso ad_format:inline"
+            // content: "kontextso ad_format:video"
             content: "kontextso ad_format:interstitial"
         )
 
